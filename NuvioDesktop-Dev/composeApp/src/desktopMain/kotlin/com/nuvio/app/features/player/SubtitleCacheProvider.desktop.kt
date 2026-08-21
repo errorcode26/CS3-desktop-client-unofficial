08c8c2225ca113ae7c6fd2d7e811b03318ecaa37
Ayu <218743986+errorcode26@users.noreply.github.com>
2026-08-21 02:52:27 +0000

    chore: remove reference directory

--- NuvioDesktop-Dev/composeApp/src/desktopMain/kotlin/com/nuvio/app/features/player/SubtitleCacheProvider.desktop.kt
+++
@@ -1,6 +0,0 @@
-package com.nuvio.app.features.player
-
-actual object SubtitleCacheProvider {
-    actual suspend fun cacheForExternalPlayer(subtitles: List<SubtitleInput>): List<SubtitleInput>? =
-        subtitles.ifEmpty { null }
-}
